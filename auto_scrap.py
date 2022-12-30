8ccf1c432a41e115d395df372a4e115e7b655b0f
chmiwo2000 <chmiwo2000@gmail.com>
2022-12-30 15:13:28 +0900

    이미지 확인 후, 클릭하는 부분 까지 완성한 파일

--- auto_scrap.py
+++ auto_scrap.py
@@ -6,29 +6,55 @@ import pandas as pd
 import pyautogui as pg
 import subprocess
 import pyperclip
-import sys
-import os
-import webbrowser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from win32comext.shell import shell # pywin32 라이브러리 설치 필요
 
+# 필요한 몇 가지 자체 함수 정의
+def confirm_image(img):
+    if pg.locateOnScreen(f'{img}.png', confidence=0.8) is None:
+        return False
+    else:
+        return True
+
+
+def image_click(name, sec, conf=0.8):
+    for i in range(1, sec, 1):
+        pg.sleep(1)
+        print(f'이미지 검색 중 {i}번째 시도 입니다.')
+        if confirm_image(name):
+            loc = pg.locateOnScreen(f'{name}.png', confidence=f'{conf}')
+            click_loc = pg.center(loc)
+            pg.moveTo(click_loc)
+            pg.click()
+            break
+        elif i == sec:
+            print(f'{name} 이미지를 찾지 못했습니다. 프로그램을 종료합니다.')
+            exit()
+        else:
+            pass
 
 
 # 관리자 권한으로 실행하는 코드이나, 적용이 잘 안됨
-print(sys.argv[-1])
-if sys.argv[-1] != 'asadmin':
-    script = os.path.abspath(sys.argv[0])
-    params = ' '.join([script] + sys.argv[1:] + ['asadmin'])
-    shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
-    sys.exit()
+# print(sys.argv[-1])
+# if sys.argv[-1] != 'asadmin':
+#     script = os.path.abspath(sys.argv[0])
+#     params = ' '.join([script] + sys.argv[1:] + ['asadmin'])
+#     shell.ShellExecuteEx(lpVerb='runas', lpFile=sys.executable, lpParameters=params)
+#     sys.exit()
 
 # 스크랩 마스터 실행
 subprocess.call(["C:\Program Files (x86)\Dahami\ScrapMaster5\SM5UpdateProgram.exe"])
-pg.sleep(10)
+image_click('login', 10)
 pg.click(717, 531)
+pg.sleep(10)
 
+# 공지사항 업데이트 여부 확인 후, Ture면 창 닫고 False면 진행
+if confirm_image('notice') == True:
+    image_click('close', 5)
+else:
+    pass
 
 # 검색 시작일자 및 종료일자 지정(당일 기준 하루 전 까지 세팅)
 from datetime import datetime, timedelta
@@ -36,7 +62,7 @@ end = datetime.now().date()
 start = end - timedelta(days=1)
 start, end = str(start), str(end)
 
-
+# 검색일자 지정
 pg.doubleClick(396, 26)
 pg.click(145, 62)
 pg.click(89, 205)
@@ -48,61 +74,71 @@ pg.hotkey('tab', interval=0.5)
 pyperclip.copy(end)
 pg.hotkey('ctrl', 'v')
 
-pg.click(158, 360)
-pg.hotkey('ctrl', 'a')
-pg.hotkey('backspace')
 
+# 검색어 타이핑 공간 마련
 search_list = ['양재 하나로', '농협유통']
 
+for n in range(0, len(search_list)):
+    pg.click(396, 26)
+    pg.click(106, 376)
+    pg.click(158, 360)
+    pg.hotkey('ctrl', 'a')
+    pg.hotkey('backspace')
+    pyperclip.copy(search_list[n])
+    pg.hotkey('ctrl', 'v', inteval=1)
+    pg.doubleClick(119, 537)
+    pg.sleep(10)
 
-pyperclip.copy(search_list[0])
-pg.hotkey('ctrl', 'v', inteval=1)
-pg.doubleClick(119, 537)
-
-pg.position()
-pg.click(462, 167, interval=1)
-pg.click(299, 997, interval=1)
-pg.click(596, 997, interval=1)
-pg.click(1234, 565, interval=1)
-pg.hotkey('enter', interval=0.5)
-
+# 엑셀 저장
+    pg.click(462, 167, interval=1)
+    pg.click(299, 997, interval=1)
+    pg.click(596, 997, interval=1)
+    pyperclip.copy(f'검색기사_2022-12-30_{search_list[n]}')
+    pg.hotkey('ctrl', 'v')
+    pg.click(1234, 565, interval=1)
+    pg.hotkey('enter', interval=0.5)
+    pg.sleep(10)
 
 # 작업할 엑셀 파일 경로 지정
-work_path = f"C:/Users/DELL9020/Desktop/검색기사_{end}.xlsx"
+    work_path = f"C:/Users/DELL9020/Desktop/검색기사_{end}_{search_list[n]}.xlsx"
 
 # 필요한 워크시트 적용
-wb = xw.Book(work_path)
-sheet = wb.sheets('기사 목록')
-print(sheet.name)
+    wb = xw.Book(work_path)
+    sheet = wb.sheets('기사 목록')
+    print(sheet.name)
 
 # 필요한 범위 선택 및 DF타입으로 변환
-_li = sheet.range('A1').expand('table').value
-_df = pd.DataFrame(_li)
+    _li = sheet.range('A1').expand('table').value
+    _df = pd.DataFrame(_li)
 
 # 매체명을 기준으로 중복 값 제거
-_df.drop_duplicates([1], keep='first', inplace=True)
+    _df.drop_duplicates([1], keep='first', inplace=True)
 
 # 접속이 필요한 URL주소만 추출
-url_li = list(_df[7])
-url_li[1:]
+    url_li = list(_df[7])
+    url_li = list(filter(None, url_li))
+    url_li[1:]
 
 # chrome으로 연결해서 url접속 확인
-chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
-webbrowser.get(chrome_path).open(url_li[1])
+    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
 
 # url접속 후, 웹페이지를 전체 캡쳐
 
 # 옵션 지정
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
 # 크롬으로 오픈 후, 해당 파일 저장
-for i in range(1, 10):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get(url_li[i])
-    width = driver.execute_script("return document.body.scrollWidth")
-    height = driver.execute_script("return document.body.scrollHeight")
-    driver.set_window_size(width, height)
-    driver.save_screenshot(f"C:/Users/DELL9020/Desktop/test/test{i}.png")
+    for i in range(1, 5):  # len(url_li) <- 테스트 끝나면 다시 돌려놓기
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
+        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get(url_li[i])
+        width = driver.execute_script("return document.body.scrollWidth")
+        height = driver.execute_script("return document.body.scrollHeight")
+        driver.set_window_size(width, height)
+        driver.save_screenshot(f"C:/Users/DELL9020/Desktop/test/{search_list[n]}test{i}.png")
+        print(i)
+
+    pg.sleep(2)
